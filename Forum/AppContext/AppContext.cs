9cd11e1cdf972eb011956bb9fa5bfa3b951661b8
Maxim Eliseev <maximum_1997@mail.ru>
2023-12-17 17:29:50 +0400

    Добавил sqlite

---
+++ Forum/AppContext/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using Forum.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forum.AppContext
+{
+    public class AppContext: DbContext
+    {
+        public AppContext()
+        {
+            DbPath = System.IO.Path.Join(@"..\", "test.db");
+        }
+        public string DbPath { get; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
---
+++ Forum/Models/Post.cs
@@ -0,0 +1,8 @@
+﻿namespace Forum.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
